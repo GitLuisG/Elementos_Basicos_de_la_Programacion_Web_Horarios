e4694102b63148c6bdeca8508a30751eb23e50f4
Luis Gerardo Perales Torres <1730505@upv.edu.mx>
2020-05-25 08:13:30 -0500

    Practica 1 Pro JS

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Elementos_B-sicos_de_la_Programaci-n_Web_Horarios
-En esta práctica se ven los elementos básicos de la programación web: HTML, CSS y Javascript. Los elementos fundamentales para la creación de páginas web. Además para ayudarnos al desarrollo web, es muy común usar frameworks y librerías para tener un desarrollo más ágil.
+# Elementos_Basicos_de_la_Programacion_Web_Horarios
+
+* En esta práctica se ven los elementos básicos de la programación web: HTML, CSS y Javascript. Los elementos fundamentales para la creación de páginas web. Además para ayudarnos al desarrollo web, es muy común usar frameworks y librerías para tener un desarrollo más ágil.
+
+* Objetivo de la práctica: El alumno demostrará el dominio de HTML, CSS y JS, además de frameworks y librerías comunes para la creación de páginas.
+
+# Instrucciones:
+* 1. Desarrollar una página HTML (horario.html) en el que con el uso de tablas recree su horario de clases. Hacer uso de archivo css para añadirle elementos de diseño. NOTA: aquí solo usar puro HTML y CSS.
+* 2. Crear una página (inputs.html), en el cual se aplique maquetado usando Bootstrap y contenga todos los elementos input HTML (ver https://www.w3schools.com/html/html_form_input_types.asp), y cada elemento input un botón que al presionarlo despliegue el valor el input en un javascript alert.
+
+* Ejemplo !["Ejemplo"](Img.png)
+* Ejemplo !["Ejemplo"](Img2.png)
--- inputs.js
+++ inputs.js
@@ -1,29 +1,149 @@
-class Inputs{
-    constructor(Alumno, Matricula, mesinicio, mesfinal, ProAca){
+
+
+class DatosAlumno{
+    constructor(Alumno, Matricula, mesinicio, mesfinal, Plan, year){
         this.nameAlumno = Alumno;
         this.matricula = Matricula;
         this.mesinicio = mesinicio;
         this.mesfinal = mesfinal;
-        this.ProAca = ProAca;
-        document.write("Conectado");
+        this.Plan = Plan;
+        this.year = year;
+        //document.write("Conectado");
+        
     }
-    
-    mostrarDatos(){
-        document.write(this.nameAlumno.value);
-        document.write(this.matricula.value);
-        document.write(this.mesinicio.value);
-        document.write(this.mesfinal.value);
-        document.write(this.ProAca.value);
+    GetAlumno(){
+        return this.nameAlumno.value;
+    }
+    GetMatricula(){
+        return this.matricula.value;
+    }
+    Getmesinicio(){
+        return this.mesinicio.value;
+    }
+    GetmesFinal(){
+        return this.mesfinal.value;
+    }
+
+    GetProAca(){
+        return this.Plan.value +"-"+ this.year;
     }
 }
 
 btnEnviar.addEventListener('click', btnEnviar_click);
-
+let inputs, i=0;
 function btnEnviar_click(e) {
     const txtAlumno = document.getElementById('name');
     const txtMatricula = document.getElementById('Matricula');
-    const txtinicio = document.getElementById('inicio');
-    const txtfin = document.getElementById('fin');
-    const txtProAca = document.getElementById('ProAca');
-    let inputs = new Inputs(txtAlumno, txtMatricula, txtinicio, txtfin, txtProAca);
+    const txtinicio = new Date(document.getElementById('inicio').value);
+    const txtfin = new Date(document.getElementById('fin').value);
+    const txtYear = 2010;
+    const txtPlan = document.getElementById('Plan');
+    inputs = new DatosAlumno(txtAlumno, txtMatricula, txtinicio.getMonth(), txtfin.getMonth(),txtPlan,txtYear);
+    document.getElementById('Panel').innerHTML =""; 
+    btnCrearTabla_click();
+    print();
+    
+}
+
+const tablaDinamicaTbody = document.getElementById('tabla-dinamica-tbody');
+//btnEnviar.addEventListener('click', btnCrearTabla_click);
+DiasSemana= new Array("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado");
+
+Materias = ["[Materia][Profesor]","Grupo",
+[
+    ["[NEGOCIOS ELECTRÓNICOS]-[MARIO HUMBERTO RODRIGUEZ CHÁVEZ]"],
+    ["[PROGRAMACIÓN WEB]-[LUIS ROBERTO FLORES DE LA FUENTE]"],
+    ["[GRAFICACIÓN POR COMPUTADORA AVANZADA]-[MANUEL RUÍZ MÉNDEZ]"],
+    ["[PROYECTOS DE TECNOLOGÍAS DE LA INFORMACIÓN]-[JOSÉ FIDENCIO LÓPEZ LUNA]"],
+    ["[ADMINISTRACIÓN DE SISTEMAS INTEGRALES]-[ISRAEL PULIDO PICAZO]"],
+    ["[MINERÍA DE DATOS APLICADA]-[MARIO ALBERTO GÓMEZ RODRÍGUEZ]"],
+    ["[INGLÉS V]-[HOMAR GARCIA GALVAN]"],
+],["ITI-07434","ITI-07435","ITI-07436","ITI-07437","ITI-07438","ITI-07439","ITI-07467"],
+[
+    ["Lunes      ","Martes     ","Miercoles  ","Jueves     ", "Viernes    ","Sabado"],
+    ["13:00-13:54","Libre      ","14:00-14:54","13:00-13:54", "Libre      ","       "],
+    ["           ","18:55-19:49","18:55-19:44","18:00-19:49", "Libre      ","       "],
+    ["           ","14:55-16:44","15:50-16:44","15:50-16:44", "15:50-16:44","       "],
+    ["15:50 16:44","Libre      ","14:55-15:49","14:55-15:49", "Libre      ","       "],
+    ["18:00 18:54","18:00-18:54","18:00-18:54","Libre      ", "Libre      ","       "],
+    ["14:00 15:49","Libre      ","Libre      ","14:00-14:54", "14:00 15:49","       "],
+    ["16:45 17:39","16:45-17:39","16:45-17:39","16:45-17:39", "16:45 17:39","       "]
+]
+];
+
+function btnCrearTabla_click(e) {
+    let r = 8;
+    let c = 9;
+    let temp=0;
+    let temp2=0;
+    let temp3=0;
+    let band = 0;
+    if (r <= 0 || c <= 0) return;
+    //tablaDinamicaTbody.innerHTML = '';  // borramos la tabla actual.
+    let rowsFragment = document.createDocumentFragment();
+    document.getElementById("alumno").innerHTML = "Alumno: "+inputs.GetAlumno();
+    document.getElementById("matricula").innerHTML = "Matricula: "+inputs.GetMatricula();
+    document.getElementById("proaca").innerHTML = "Programa Academico: INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN";
+    document.getElementById("plan").innerHTML = "Plan de estudio: "+inputs.GetProAca();
+    document.getElementById("sede").innerHTML = "Sede: Victoria";
+    for (let ir = 0; ir < r; ir++) {
+
+        // Creamos un nodo tr (row) al cual vamos a agregar los nodos td.
+        let tr = document.createElement('tr');
+
+        // Creamos los nodos td y los agregamos al tr.
+        for (let ic = 0; ic < c; ic++) {
+            let td = document.createElement('td');
+            td.id = 'td_' + ir + '_' + ic;
+            td.classList.add('td-m');
+            td.textContent = `[${ir}][${ic}]`;  // string interpolation
+            if(band==ir && ic != 0 && ic != 1 && ic < 8){
+                td.textContent = `${Materias[4][band][temp3]}`;
+                temp3++;
+            }
+            if(ic==8) td.textContent = `    4`;
+            
+            if(ir == 0 && ic == 0){
+                td.textContent = `[Materia] [Profesor]`;
+            }
+            if(ir == 0 && ic == 1){
+                td.textContent = `Grupo`;
+            }
+            if(ir == 0 && ic != 0 && ic != 1 && i < DiasSemana.length){
+                if(temp2 < 8){
+                    td.textContent = `${Materias[4][0][temp2++]}`;
+                }
+            }
+            if(ir==0 && ic==8){
+                td.textContent = `Creditos`;
+            }
+            if(ic == 0 && ir != 0){
+                td.textContent = Materias[2][temp];
+
+            }
+            if(ic == 1 && ir != 0){
+                td.textContent = Materias[3][temp];
+                temp++;
+            }
+
+            
+            td.addEventListener('click', tablaDinamicaTd_click);  // click event
+            tr.appendChild(td);
+        }
+        temp3=0;
+        band++;
+        // Agregamos el tr creado (que dentro tiene los td) al fragment.
+        rowsFragment.appendChild(tr);
+
+    }
+
+    // Agregamos los nodos tr del fragment al tbody de la tabla dinámica.
+    tablaDinamicaTbody.appendChild(rowsFragment);
+
+}
+
+function tablaDinamicaTd_click(e) {
+    const td = this;
+    td.style.backgroundColor = 'red';
+    alert('Hizo click en ' + td.id);
 }
